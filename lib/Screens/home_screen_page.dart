b4abeb183cc2eba2f46c5122534a0f06e1d1c350
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-05-04 23:42:18 +0530

    design bottom navigation bar in home page

--- lib/Screens/home_screen_page.dart
+++ lib/Screens/home_screen_page.dart
@@ -17,88 +17,136 @@ class _HomePageScreen extends State<HomeScreenPage> {
           systemOverlayStyle: SystemUiOverlayStyle.light,
         ),
         body: Container(
-            color: Colors.white,
-            child: Column(
+          color: Colors.white,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              verticalDirection: VerticalDirection.down,
               children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Container(
-                      height: 20,
-                      child: Row(
-                        children: <Widget>[
-                          Image.asset(
-                            "youtube_full.png",
-                            height: 100,
-                            width: 50,
-                          ),
-                          Text(
-                            "Youtube",
-                            style: TextStyle(color: Colors.black, fontSize: 20),
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      width: 50,
-                    ),
-                    Container(
-                      alignment: Alignment.centerRight,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          IconButton(
-                              onPressed: () => {print("object")},
-                              icon: Icon(Icons.cast_connected)),
-                          IconButton(
-                              onPressed: () => {print("object")},
-                              icon: Icon(Icons.notifications)),
-                          IconButton(
-                              onPressed: () => {print("object")},
-                              icon: Icon(Icons.search_rounded)),
-                          IconButton(
-                              onPressed: () => {print("object")},
-                              icon: Icon(Icons.person)),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
                 Container(
-                  margin: EdgeInsets.only(top: 70),
-                  child: Column(children: <Widget>[
-                    Image.asset(
-                      "offline.jpeg",
-                      height: 250,
-                      width: 250,
-                    ),
-                    Text(
-                      "You're offline",
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "Watch downloaded videos of find new\nvideos to download in your Library",
-                      style: TextStyle(),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    FlatButton(
-                        color: Color.fromARGB(255, 48, 162, 255),
-                        onPressed: () => {print("object")},
-                        child: Text("GO TO DOWNLOaDS")),
-                    TextButton(
-                        onPressed: () => {print("object")},
-                        child: Text("Retry"))
-                  ]),
-                )
-              ],
-            )));
+                    color: Colors.white,
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              height: 20,
+                              child: Row(
+                                children: <Widget>[
+                                  Image.asset(
+                                    "youtube_full.png",
+                                    height: 100,
+                                    width: 50,
+                                  ),
+                                  const Text(
+                                    "Youtube",
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 20),
+                                  )
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 50,
+                            ),
+                            Container(
+                              alignment: Alignment.centerRight,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  IconButton(
+                                      onPressed: () => {print("object")},
+                                      icon: Icon(Icons.cast_connected)),
+                                  IconButton(
+                                      onPressed: () => {print("object")},
+                                      icon: Icon(Icons.notifications)),
+                                  IconButton(
+                                      onPressed: () => {print("object")},
+                                      icon: Icon(Icons.search_rounded)),
+                                  IconButton(
+                                      onPressed: () => {print("object")},
+                                      icon: Icon(Icons.person)),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 100),
+                          child: Column(children: <Widget>[
+                            Image.asset(
+                              "offline.jpeg",
+                              height: 180,
+                              width: 250,
+                            ),
+                            const Text(
+                              "You're offline",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            const Text(
+                              "Watch downloaded videos of find new\nvideos to download in your Library",
+                              style: TextStyle(fontSize: 15),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            FlatButton(
+                                color: Color.fromARGB(255, 1, 18, 179),
+                                onPressed: () => {print("object")},
+                                child: Text(
+                                  "GO TO DOWNLOADS",
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                            TextButton(
+                                onPressed: () => {print("object")},
+                                child: Text(
+                                  "RETRY",
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 0, 96, 175)),
+                                ))
+                          ]),
+                        ),
+                      ],
+                    )),
+                Container(
+                    color: Colors.white,
+                    child: Column(
+                      children: <Widget>[
+                        Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            IconButton(
+                                onPressed: () => {print("object")},
+                                icon: Icon(Icons.home)),
+                            IconButton(
+                                onPressed: () => {print("object")},
+                                icon: Icon(Icons.video_label_rounded)),
+                            GestureDetector(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20)),
+                                child: Icon(Icons.add),
+                              ),
+                            ),
+                            IconButton(
+                                onPressed: () => {print("object")},
+                                icon: Icon(Icons.search_rounded)),
+                            IconButton(
+                                onPressed: () => {print("object")},
+                                icon: Icon(Icons.person)),
+                          ],
+                        ),
+                      ],
+                    ))
+              ]),
+        ));
   }
 }
