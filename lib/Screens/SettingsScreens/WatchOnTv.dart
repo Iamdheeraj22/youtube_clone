587598be2d846d1f12ab38599205ff5c95c90647
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-05-14 23:59:28 +0530

    Watch On tv  design screen inprogress

--- lib/Screens/SettingsScreens/WatchOnTv.dart
+++ lib/Screens/SettingsScreens/WatchOnTv.dart
@@ -11,42 +11,76 @@ class _WatchTv extends State<WatchTvPage> {
     // TODO: implement build
     return Scaffold(
       body: SafeArea(
-          child: Container(
-        margin: EdgeInsets.only(top: 20),
-        child: Column(
-          children: [
-            Row(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              IconButton(
+                  onPressed: () => {},
+                  icon: Icon(
+                    Icons.arrow_back,
+                    size: 33,
+                  )),
+              SizedBox(
+                width: 25,
+              ),
+              Text(
+                "Watch on TV",
+                style: TextStyle(color: Colors.black, fontSize: 25),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            color: Colors.red,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 30, bottom: 30, left: 30, right: 39),
+              child: Text(
+                "Connect to the same Wi-Fi network as your\nTV",
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 25.0, left: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
-                  width: 10,
+                Text(
+                  "No Tvs found",
+                  style: TextStyle(fontSize: 20),
                 ),
-                IconButton(
-                    onPressed: () => {},
-                    icon: Icon(
-                      Icons.arrow_back,
-                      size: 33,
-                    )),
                 SizedBox(
-                  width: 25,
+                  height: 15,
                 ),
                 Text(
-                  "Watch on TV",
-                  style: TextStyle(color: Colors.black, fontSize: 25),
+                    "Check your phone settings and try again if that doesn't work,\nyou can also link your TV and phone using a TV code."),
+                SizedBox(
+                  height: 15,
+                ),
+                TextButton(
+                    onPressed: () => {}, child: Text("OPEN WI-FI SETTINGS")),
+                SizedBox(
+                  height: 50,
+                ),
+                Divider(
+                  color: Colors.black,
+                  height: 2,
                 )
               ],
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              color: Colors.red,
-              child: Text(
-                "Connect to the same Wi-Fi network as your\nTV",
-                style: TextStyle(fontSize: 18, color: Colors.black),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       )),
     );
   }
